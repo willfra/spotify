2c12515a6a8930c869b3cf868de767fef90a7165
William França <61748715+willfra@users.noreply.github.com>
2021-09-28 17:22:14 -0300

    adição player

---
+++ main.js
@@ -0,0 +1,5 @@
+var playpause = document.querySelector(".button_play_pause);
+
+hamburguer.addEventListener("click", function () {
+    document.querySelector(".container").classList.toggle("show-menu");
+});
